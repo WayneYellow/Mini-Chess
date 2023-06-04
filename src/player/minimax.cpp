d7a9d3baeafe870188d33f9d2473bcac9e19c218
yellowwayne <warden.e.nelson@gmail.com>
2023-06-04 21:22:46 +0800

    done with alpha beta pruning

--- src/player/minimax.cpp
+++ src/player/minimax.cpp
@@ -41,7 +41,7 @@ void read_board(std::ifstream& fin) {
 void write_valid_spot(std::ofstream& fout) {
   // Keep updating the output until getting killed.
   while(true) {
-    auto move = Minimax::get_move(root, 4);
+    auto move = Minimax::get_move(root, 3);
     fout << move.first.first << " " << move.first.second << " "\
          << move.second.first << " " << move.second.second << std::endl;
     
--- src/policy/alpha-beta-pruning.cpp
+++ src/policy/alpha-beta-pruning.cpp
@@ -4,7 +4,7 @@
 #include "./alpha-beta-pruning.hpp"
 
 /**
- * @brief Minimax algorithm
+ * @brief Minimax algorithm with alpha-beta pruning
  * 
  * @param state Now state
  * @param depth 
@@ -14,33 +14,54 @@ Move AlphaBetaPruning::get_move(State *state, int depth){
   if(!state->legal_actions.size())
     state->get_legal_actions();
   
-  int max = -1000000;
-  Move result;
-  if(depth == 0 || state->game_state == WIN){
-    result = state->legal_actions[0];
-  }
-  else{
-    for(auto action: state->legal_actions){
-    auto next_state = state->next_state(action);
-    int value = search(next_state, depth-1, false, -1000000, 1000000);
-      if(value > max){
-        max = value;
-        result = action;
+  if(state->player == 0){
+    int max = -1000000;
+    Move result;
+    if(depth == 0 || state->game_state == WIN){
+      result = state->legal_actions[0];
+    }
+    else{
+      for(auto action: state->legal_actions){
+        auto next_state = state->next_state(action);
+        int value = search(next_state, depth-1, false, -1000000, 1000000);
+        if(value > max){
+          max = value;
+          result = action;
+        }
       }
     }
+    return result;
   }
-  return result;
+  else{
+    int min = 1000000;
+    Move result;
+    if(depth == 0 || state->game_state == WIN){
+      result = state->legal_actions[0];
+    }
+    else{
+      for(auto action: state->legal_actions){
+        auto next_state = state->next_state(action);
+        int value = search(next_state, depth-1, true, -1000000, 1000000);
+        if(value < min){
+          min = value;
+          result = action;
+        }
+      }
+    }
+    return result;
+  }
 }
 
 int AlphaBetaPruning::search(State *state, int depth, bool isMax, int alpha, int beta){
-  if(depth == 0){
-    return state->evaluate();
-  }
+  
   if(state->game_state == WIN && isMax){
     return 1000000;
   }
   else if(state->game_state == WIN && !isMax){
     return -1000000;
+  }
+  if(depth == 0){
+    return state->evaluate();
   }
   if(isMax){
     int max = -1000000;
